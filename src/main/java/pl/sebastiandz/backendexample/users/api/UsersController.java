a048493c19d16d33eac92718a1dd89defe960312
Sebastian Dziak <sebastian.dziak@orange.com>
2021-07-14 22:13:44 +0200

    Update users controller

--- src/main/java/pl/sebastiandz/backendexample/users/api/UsersController.java
+++ src/main/java/pl/sebastiandz/backendexample/users/api/UsersController.java
@@ -1,19 +1,38 @@
 package pl.sebastiandz.backendexample.users.api;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sebastiandz.backendexample.calculations.CalculationService;
+import pl.sebastiandz.backendexample.github.GitHubFacade;
+import pl.sebastiandz.backendexample.github.dto.GitHubUserDto;
 import pl.sebastiandz.backendexample.users.dto.UserDto;
 
 @RestController
 @RequestMapping("users")
+@AllArgsConstructor
 public class UsersController {
+
+    private final GitHubFacade gitHubFacade;
+    private final CalculationService calculationService;
 
     @GetMapping(path = "{login}")
     UserDto userDetails(@PathVariable("login") String login) {
 
-        //TODO temporary mocked response
-        return UserDto.builder().login(login).build();
+        GitHubUserDto gitHubUser = gitHubFacade.user(login);
+        double calculations = calculationService.calculate(gitHubUser.getFollowers(), gitHubUser.getPublicRepos());
+
+        return UserDto
+                .builder()
+                .id(gitHubUser.getId())
+                .login(gitHubUser.getLogin())
+                .name(gitHubUser.getName())
+                .type(gitHubUser.getType())
+                .avatarUrl(gitHubUser.getAvatarUrl())
+                .createdAt(gitHubUser.getCreatedAt())
+                .calculations(calculations)
+                .build();
     }
 }
