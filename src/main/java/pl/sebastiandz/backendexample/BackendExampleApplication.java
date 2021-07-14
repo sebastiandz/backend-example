9a0df4d61e6a03ede45af0d72ac2446fae114c74
Sebastian Dziak <sebastian.dziak@orange.com>
2021-07-14 16:42:01 +0200

    Spring Boot application generated by start.spring.io

---
+++ src/main/java/pl/sebastiandz/backendexample/BackendExampleApplication.java
@@ -0,0 +1,13 @@
+package pl.sebastiandz.backendexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sebastiandz/backendexample/BackendExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sebastiandz.backendexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
